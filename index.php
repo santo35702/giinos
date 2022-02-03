cc232d32002cab4e846e3180e72967be5a14eb75
MD: Suvo <santo35702@gmail.com>
2022-02-03 17:30:49 +0600

    Font-Awesome CSS link added in index file

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-xcompatible" content="IE-edge">
+        <title>GIINOS-Software Intelligence</title>
+
+        <!-- Google Fonts -->
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;700;900&display=swap" rel="stylesheet">
+        <!-- Font-Awesome Free CSS -->
+        <link rel="stylesheet" href="node_modules/fontawesome-free/css/all.css">
+    </head>
+    <body>
+        <h1>heading</h1>
+    </body>
+</html>
