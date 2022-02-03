8e24d164d7fd9d070ccac4241b50947c7b677106
MD: Suvo <santo35702@gmail.com>
2022-02-03 14:38:46 +0600

    header section done with a image

--- index.php
+++ index.php
@@ -9,57 +9,56 @@
         <!-- Google Fonts -->
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
         <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;700;900&display=swap" rel="stylesheet">
+        <script src="https://kit.fontawesome.com/97d7f244c6.js" crossorigin="anonymous"></script>
         <!-- Required CSS -->
         <link rel="stylesheet" href="vendor/css/normalize.css">
         <link rel="stylesheet" href="vendor/css/grid.css">
         <link rel="stylesheet" href="vendor/css/animated.circle.css">
         <!-- Bootstrap CSS -->
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
-        <!-- <link rel="stylesheet" href="resource/css/style.css?v=1.0"> -->
+        <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"> -->
+        <link rel="stylesheet" href="resource/css/style.css?v=1.0">
         <link rel="stylesheet" href="resource/css/responsive.css">
     </head>
     <body>
         <!-- start header section-->
-        <nav class="navbar navbar-expand-lg navbar-light bg-light">
-            <div class="container-fluid">
-                <a class="navbar-brand" href="#">Navbar</a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                        <li class="nav-item">
-                            <a class="nav-link active" aria-current="page" href="#">Home</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">Link</a>
-                        </li>
-                        <li class="nav-item dropdown">
-                            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                Dropdown
-                            </a>
-                            <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                                <li><a class="dropdown-item" href="#">Action</a></li>
-                                <li><a class="dropdown-item" href="#">Another action</a></li>
-                                <li>
-                                    <hr class="dropdown-divider">
-                                </li>
-                                <li><a class="dropdown-item" href="#">Something else here</a></li>
-                            </ul>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link disabled">Disabled</a>
-                        </li>
+        <header>
+
+            <nav>
+                <div class="row" id="home">
+
+                    <a href="./">
+                        <img src="resource/img/logo.png" alt="Giinos" class="logo">
+                    </a>
+                    <ul class="main-nav">
+                        <li class="active"><a href="./">Home</a></li>
+                        <li><a href="#service">What we do</a></li>
+                        <li><a href="#success">Success Story</a></li>
+                        <li><a href="#team">Team and Culture</a></li>
+                        <li><a href="#hire">🔥We’re hiring</a></li>
+                        <li><a href="#contact">Contact Us</a></li>
                     </ul>
-                    <form class="d-flex">
-                        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-                        <button class="btn btn-outline-success" type="submit">Search</button>
-                    </form>
+                    <div class="dropdown">
+                        <button class="dropbtn"><img src="resource/img/Vector.png" alt=""> En <img src="resource/img/Forward.png" alt=""></button>
+                        <div class="dropdown-content">
+                            <a href="#">Thai</a>
+                        </div>
+                    </div>
+                </div>
+            </nav>
+
+            <div class="row">
+                <div class="col span_1_of_3 box">
+                    <h1>Intelligent <span class="fluid">Fluid</span> <br> Process Flow <br>Management <br> Platform</h1>
+                    <p class="hero-description">for maximizing <br> <span class="hero-p"> holistic values</span></p>
+                </div>
+                <div class="col span_1_of_3">
+                    <img class="hero_img" src="resource/img/Group_810.png" alt="hero">
                 </div>
             </div>
-        </nav>
+
+        </header>
 
         <!-- Option 1: Bootstrap Bundle with Popper -->
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+        <!-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script> -->
     </body>
 </html>
