0b0cabc3c6fbea61048b186a1a55a6e1f825bb4c
MD: Suvo <santo35702@gmail.com>
2022-02-03 14:02:00 +0600

    html template set

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-xcompatible" content="IE-edge">
+        <title>GIINOS-Software Intelligence</title>
+
+        <!-- Google Fonts -->
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;700;900&display=swap" rel="stylesheet">
+        <!-- Required CSS -->
+        <link rel="stylesheet" href="vendor/css/normalize.css">
+        <link rel="stylesheet" href="vendor/css/grid.css">
+        <link rel="stylesheet" href="vendor/css/animated.circle.css">
+        <link rel="stylesheet" href="resource/css/style.css?v=1.0">
+        <link rel="stylesheet" href="resource/css/responsive.css">
+    </head>
+    <body>
+
+    </body>
+</html>
